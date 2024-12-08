e2dc5cf67209eff368b37fbe863d9c549e96bd3f
Aleksandrs Zeltisevs <azeltisevs@gmail.com>
2024-12-08 12:31:01 +0200

    day 8 task 1

---
+++ 8/1.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"advent_of_code_2024/helpers"
+	"fmt"
+)
+
+type AntinodePoint struct {
+	helpers.Point
+	char rune
+}
+
+func main() {
+	lines := helpers.ReadAllLinesFromFile("./8/input.txt")
+	n, m := len(lines), len(lines[0])
+	antennas := helpers.FindAllAlphanumeric(lines)
+
+	helpers.PrintField(antennas, n, m)
+
+	antinodes := make(map[helpers.Point]int)
+	var sum int
+	for antenna, points := range antennas {
+		fmt.Println("antenna", antenna)
+		for i := range points {
+			for j := i + 1; j < len(points); j++ {
+				dx1, dy1 := points[i].X-points[j].X, points[i].Y-points[j].Y
+				dx2, dy2 := points[j].X-points[i].X, points[j].Y-points[i].Y
+				fmt.Println(points[i], points[j])
+				fmt.Println("dx1", dx1, "dy1", dy1)
+				fmt.Println("dx2", dx2, "dy2", dy2)
+
+				antinode1 := helpers.Point{points[i].Y + dy1, points[i].X + dx1}
+				if checkAntinode(antinode1, m, n) {
+					point := helpers.Point{
+						antinode1.Y, antinode1.X,
+					}
+					antinodes[point]++
+
+					//if _, ok := antinodes[point]; !ok {
+					//	antinodes[point] = 1
+					//} else {
+					//}
+				}
+				antinode2 := helpers.Point{points[j].Y + dy2, points[j].X + dx2}
+				if checkAntinode(antinode2, m, n) {
+					point := helpers.Point{
+						Y: antinode2.Y,
+						X: antinode2.X,
+					}
+					antinodes[point]++
+				}
+			}
+		}
+	}
+
+	//for point := range antinodes {
+	//	antennas['#'] = append(antennas['#'], point.Point)
+	//}
+	//helpers.PrintField(antennas, n, m)
+
+	fmt.Println("sum", sum)
+	fmt.Println(len(antinodes))
+}
+
+func checkAntinode(antinode1 helpers.Point, m int, n int) bool {
+	return antinode1.X >= 0 && antinode1.X < m &&
+		antinode1.Y >= 0 && antinode1.Y < n
+}
--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,10 +1,12 @@
 package helpers
 
 import (
+	"fmt"
 	"os"
 	"regexp"
 	"strconv"
 	"strings"
+	"unicode"
 )
 
 func ReadAllLinesFromFile(filename string) []string {
@@ -20,6 +22,23 @@ func ExtractAllNumbers(lines []string) [][]int {
 	var result [][]int
 	for _, line := range lines {
 		result = append(result, ExtractNumbers(line))
+	}
+	return result
+}
+
+type Point struct {
+	Y int
+	X int
+}
+
+func FindAllAlphanumeric(lines []string) map[rune][]Point {
+	result := make(map[rune][]Point)
+	for y, line := range lines {
+		for x, r := range line {
+			if unicode.IsLetter(r) || unicode.IsDigit(r) {
+				result[r] = append(result[r], Point{y, x})
+			}
+		}
 	}
 	return result
 }
@@ -40,5 +59,39 @@ func ExtractNumbers(s string) []int {
 		result = append(result, x)
 	}
 
+	return result
+}
+
+func PrintField(field map[rune][]Point, n int, m int) {
+	inversedField := inverseField(field)
+	fmt.Println()
+	fmt.Printf("%3c", ' ')
+	for x := 0; x < n; x++ {
+		fmt.Printf("%3d", x)
+	}
+	fmt.Println()
+	for y := 0; y < n; y++ {
+		fmt.Printf("%3d", y)
+		for x := 0; x < m; x++ {
+			if char, ok := inversedField[Point{y, x}]; ok {
+				fmt.Printf("%3c", char)
+			} else {
+				fmt.Printf("%3c", '.')
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func inverseField(field map[rune][]Point) map[Point]rune {
+	result := make(map[Point]rune)
+	for k, v := range field {
+		for _, point := range v {
+			if value, ok := result[point]; ok {
+				panic(fmt.Sprintf("conflict. key %v, existing value: %c, new value: %c", point, value, k))
+			}
+			result[point] = k
+		}
+	}
 	return result
 }
